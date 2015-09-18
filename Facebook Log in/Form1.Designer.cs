4a3405805acf3fb50030be78d0b5e767b04ed5dc
Mohamed Emam <mohamedemam0@outlook.com>
2015-09-18 13:29:07 +0200

    Files
    Files..

---
+++ Facebook Log in/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Facebook_Log_in
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webFacebook = new System.Windows.Forms.WebBrowser();
+            this.btnSignIn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // webFacebook
+            // 
+            this.webFacebook.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webFacebook.Location = new System.Drawing.Point(0, 0);
+            this.webFacebook.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webFacebook.Name = "webFacebook";
+            this.webFacebook.Size = new System.Drawing.Size(793, 435);
+            this.webFacebook.TabIndex = 0;
+            this.webFacebook.DocumentCompleted += new System.Windows.Forms.WebBrowserDocumentCompletedEventHandler(this.webFacebook_DocumentCompleted);
+            // 
+            // btnSignIn
+            // 
+            this.btnSignIn.Location = new System.Drawing.Point(366, 357);
+            this.btnSignIn.Name = "btnSignIn";
+            this.btnSignIn.Size = new System.Drawing.Size(75, 23);
+            this.btnSignIn.TabIndex = 1;
+            this.btnSignIn.Text = "Sing In";
+            this.btnSignIn.UseVisualStyleBackColor = true;
+            this.btnSignIn.Click += new System.EventHandler(this.btnSignIn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(793, 435);
+            this.Controls.Add(this.btnSignIn);
+            this.Controls.Add(this.webFacebook);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser webFacebook;
+        private System.Windows.Forms.Button btnSignIn;
+    }
+}
+
---
+++ Facebook Log in/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Facebook;
+
+namespace Facebook_Log_in
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignIn_Click(object sender, EventArgs e)
+        {
+            string OAuthURL = @"https://www.facebook.com/dialog/oauth?client_id=323356637789373&redirect_uri= https://www.facebook.com/connect/login_success.html&response_type=token";
+            webFacebook.Navigate(OAuthURL);
+        }
+
+        private void webFacebook_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            string url1 = webFacebook.Url.AbsoluteUri;
+            string url2 = url1.Substring(url1.IndexOf("access_token") + 13);
+            string access_token = url2.Substring(0, url2.IndexOf("&"));
+            MessageBox.Show("access_toke = "+ access_token);
+
+            FacebookClient fb = new FacebookClient(access_token);
+
+            dynamic data = fb.Get("/me");
+
+            MessageBox.Show("id="+data.id + Environment.NewLine + "name="+data.name);
+
+        }
+    }
+}
